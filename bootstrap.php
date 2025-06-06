f4f9179256a2135707eda3b009b144f571abf2c2
kassantw <wichack.thomas@gmail.com>
2025-06-06 10:43:49 +0200

    ADD Database - phpdotenv - Test conexion

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,11 @@
 <?php
 
 // fin mise en place autoload
+
+use Dotenv\Dotenv;
+
 require_once __DIR__ . '/vendor/autoload.php';
+
+// Initialisation de la librairie
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
--- public/index.php
+++ public/index.php
@@ -3,12 +3,23 @@
 // Mise en place autoload
 
 use App\core\Router;
+use App\core\Database; // 👈 ajoute cette ligne
+
 
 require_once __DIR__ . '/../bootstrap.php';
 
-$router = new Router();
+try
+    {$router = new Router();
+
+$db = Database::getConnexion();
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $method = $_SERVER['REQUEST_METHOD'];
 
 $router->dispatch($uri, $method);
+} catch (Exception $e) {
+    return $json = json_encode([
+        'error' => 'Une erreur est survenue',
+        'message' => $e->getMessage()
+    ]);
+}
